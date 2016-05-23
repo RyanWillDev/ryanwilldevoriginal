92d894403f6e8450fc88c68e0bfee2b68668bbdb
Ryan  Will <ryanwilldev@gmail.com>
2016-05-23 14:50:44 -0500

    Added and styled chevrons to the nav links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,33 @@ class About extends React.Component {
       <div className="about-container">
         <p className="about-description"><span className="emphasis">Ryan Will</span>,
           an aspiring Web Developer from the the Nashville area.
-          I am a natural and determined problem solver. I enjoy solving new challenges, and I am
-          constantly looking to fill the gaps in my knowledge of any topic.
+          A natural and determined problem solver, I enjoy solving new challenges, and I am
+          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+          look through my projects and reach out if you want to work together.
+        </p>
+        <p className="about-description"><span className="emphasis">Ryan Will</span>,
+          an aspiring Web Developer from the the Nashville area.
+          A natural and determined problem solver, I enjoy solving new challenges, and I am
+          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+          look through my projects and reach out if you want to work together.
+        </p>
+        <p className="about-description"><span className="emphasis">Ryan Will</span>,
+          an aspiring Web Developer from the the Nashville area.
+          A natural and determined problem solver, I enjoy solving new challenges, and I am
+          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+          look through my projects and reach out if you want to work together.
+        </p>
+        <p className="about-description"><span className="emphasis">Ryan Will</span>,
+          an aspiring Web Developer from the the Nashville area.
+          A natural and determined problem solver, I enjoy solving new challenges, and I am
+          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+          look through my projects and reach out if you want to work together.
+        </p>
+        <p className="about-description"><span className="emphasis">Ryan Will</span>,
+          an aspiring Web Developer from the the Nashville area.
+          A natural and determined problem solver, I enjoy solving new challenges, and I am
+          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+          look through my projects and reach out if you want to work together.
         </p>
         <Nav left="Projects" right="Contact" />
       </div>
--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -5,9 +5,23 @@ import { Link } from 'react-router';
 const NavLink = (props) => {
   const floatClass = ` float float-${props.direction}`;
   const path = `/${props.componentName.toLowerCase()}`;
+  const arrowClass = `fa fa-chevron-${props.direction}`;
+
+  function createMarkup() {
+    if (props.direction === 'left') {
+      return { __html: `<i class="${arrowClass}"></i>
+      ${props.componentName}` };
+    }
+
+    if (props.direction === 'right') {
+      return { __html: `${props.componentName}
+      <i class="${arrowClass}"></i>` };
+    }
+  }
+
   return (
     <div className={floatClass}>
-      <Link to={path}>{props.componentName}</Link>
+      <Link to={path} dangerouslySetInnerHTML={createMarkup()} />
     </div>
   );
 };
