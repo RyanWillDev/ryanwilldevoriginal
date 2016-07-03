8fce87ca41acc32702d319416a9e111e8e4cbbd8
Ryan  Will <ryanwilldev@gmail.com>
2016-07-03 15:36:37 -0500

    centered description on About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,12 +8,14 @@ class About extends React.Component {
   render() {
     return (
       <div className="about-container">
-        <p className="about-description"><span className="emphasis">Ryan Will</span>,
-          an aspiring Web Developer from the the Nashville area.
-          A natural and determined problem solver, I enjoy solving new challenges, and I am
-          constantly looking to fill the gaps in my knowledge of any topic. Feel free to
-          look through my projects and reach out if you want to work together.
-        </p>
+        <div className="about-desc-container">
+          <p className="about-description"><span className="emphasis">Ryan Will</span>,
+            an aspiring Web Developer from the the Nashville area.
+            A natural and determined problem solver, I enjoy solving new challenges, and I am
+            constantly looking to fill the gaps in my knowledge of any topic. Feel free to
+            look through my projects and reach out if you want to work together.
+          </p>
+        </div>
         <Nav left="Projects" right="Contact" />
       </div>
     );
